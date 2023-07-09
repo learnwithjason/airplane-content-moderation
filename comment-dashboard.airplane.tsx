0d901957785578ca76a348871d678ac89e6349ce
Jason Lengstorf <jason@lengstorf.com>
2023-07-08 22:27:02 -0700

    feat: final form for tutorial

--- comment-dashboard.airplane.tsx
+++ comment-dashboard.airplane.tsx
@@ -5,7 +5,6 @@ import {
 	useComponentState,
 	Checkbox,
 	CheckboxState,
-	Button,
 	useTaskQuery,
 } from '@airplane/views';
 import airplane from 'airplane';
@@ -13,40 +12,47 @@ import airplane from 'airplane';
 // Views documentation: https://docs.airplane.dev/views/getting-started
 const CommentDashboard = () => {
 	const { id, checked } = useComponentState<CheckboxState>();
-	const { refetch } = useTaskQuery({ slug: 'list_comments' });
+	const unflagged = useTaskQuery({ slug: 'list_unflagged_comments' });
+	const flagged = useTaskQuery({ slug: 'list_flagged_comments' });
 
 	return (
 		<Stack>
 			<Heading>Comment Moderation</Heading>
+			<Table
+				title="Approved Comments"
+				task={'list_unflagged_comments'}
+				defaultPageSize={20}
+				hiddenColumns={['id', 'flagged']}
+				rowActions={{
+					slug: 'flag_comment',
+					label: 'flag',
+					onSuccess: () => flagged.refetch(),
+				}}
+			/>
 			<Checkbox
 				id={id}
 				label="Show flagged comments (view at your own risk!)"
 			/>
-			<Table
-				title="All Comments"
-				task="list_comments"
-				defaultPageSize={30}
-				hiddenColumns={['id']}
-				outputTransform={(data) =>
-					data.filter((row) => checked || !row.flagged)
-				}
-				rowActions={({ row }) => {
-					const action = row.flagged ? 'unflag' : 'flag';
-
-					return (
-						<Button
-							variant="subtle"
-							task={{
-								slug: `${action}_comment`,
-								params: { id: row.id },
-								onSuccess: () => refetch(),
-							}}
-						>
-							{action}
-						</Button>
-					);
-				}}
-			/>
+			{checked ? (
+				<Table
+					title="Flagged Comments"
+					task="list_flagged_comments"
+					hiddenColumns={['id']}
+					rowActions={[
+						{
+							slug: 'unflag_comment',
+							label: 'unflag',
+							onSuccess: () => unflagged.refetch(),
+						},
+						{
+							slug: 'delete_comment',
+							label: 'delete',
+							confirm: true,
+							onSuccess: () => unflagged.refetch(),
+						},
+					]}
+				/>
+			) : null}
 		</Stack>
 	);
 };
